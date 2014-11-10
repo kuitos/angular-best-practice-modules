1194471a54649afd813b60c581915592b407b877
刘 奎 <kui.liu@shuyun.com>
2014-11-10 13:42:42 +0800

    http-handler优化。loading状态等全部使用http interceptor拦截处理，加入saving状态。http请求头加入cache-control禁用浏览器缓存

--- js/http-handler.js
+++ js/http-handler.js
@@ -15,95 +15,162 @@
         .config(["$httpProvider", function ($httpProvider) {
 
             var
-                /** loading状态切换 */
+                /** http请求相关状态(loading,saving)切换 */
                 count = 0,
                 loading = false,
+                saving = false,
+
                 stopLoading = function () {
                     count = 0;
                     loading = false;
-                    _app.loading(false); // end loading
+                    saving = false;
+                    _app.isLoading(false); // end loading
+                    _app.isSaving(false); // end saving
                 };
 
             /*************************** http超时时间设为30s ***************************/
             $httpProvider.defaults.timeout = 30 * 1000;
+            /*************************** 禁用浏览器缓存 ***************************/
+            $httpProvider.defaults.headers.common["Cache-Control"] = "no-cache";
             /* 广告时间哈哈.... */
             $httpProvider.defaults.headers.common["X-Requested-With"] = "https://github.com/kuitos";
 
-            /************************* 根据http请求状态判断是否需要loading图标 *************************/
-            $httpProvider.defaults.transformRequest.push(function (data) {  // global loading start
-                count += 1;
-                if (!loading) {
-                    _app.timeout(function () {
-                        if (!loading && count > 0) {
-                            loading = true;
-                            _app.loading(true);
-                        }
-                    }, 500); // if no response in 500ms, begin loading
-                }
-                return data;
-            });
-            $httpProvider.defaults.transformResponse.push(function (data) { // global loading end
-                count -= 1;
-                if (loading && count === 0) {
-                    stopLoading();
-                }
-                return data;
-            });
+            /******************** http拦截器，用于统一处理错误信息、消息缓存、请求响应状态、响应结果处理等 **********************/
+            $httpProvider.interceptors.push(["$q", "$log", "$timeout", "$cacheFactory", "tipsHandler",
+                function ($q, $log, $timeout, $cacheFactory, tipsHandler) {
 
-            /******************** http拦截器，用于统一处理错误信息、消息缓存、响应结果处理等 **********************/
-            $httpProvider.interceptors.push(["$q", "$log", function ($q, $log) {
+                    return {
 
-                return {
+                        request: function (config) {
 
-                    response: function (res) {
-                        var config = res.config,
-                            responseBody = res.data,
-                            cache;
+                            count++;
+                            if (!loading) {
 
-                        if (angular.isObject(responseBody) && !responseBody.success) {
+                                // saving start
+                                if (config.saveStatus) {
+                                    saving = true;
+                                    _app.isSaving(true);
+                                }
 
-                            $log.error("%s 接口请求错误：%s", config.url, responseBody.message);
-                            return $q.reject(res);
-
-                        } else {
-
-                            // 自定义配置，用于query请求直接返回data部分
-                            if (config.useDataDirect) {
-                                res.data = responseBody.data;
+                                $timeout(function () {
+                                    if (!loading && count > 0) {
+                                        loading = true;
+                                        _app.isLoading(true);
+                                    }
+                                }, 500); // if no response in 500ms, begin loading
                             }
 
-                            // 自定义配置，若该请求成功后需要重新刷新cache(save,update,delete等操作)，则清空对应cache。angular默认cache为$http
-                            if (config.refreshCache) {
-                                cache = angular.isObject(config.cache) ? config.cache : _app.cacheFactory.get("$http");
-                                cache.removeAll();
+                            return config;
+                        },
+
+                        requestError: function (rejection) {
+                            $log.error("%s 接口请求失败!", rejection.url);
+                            return $q.reject(rejection);
+                        },
+
+                        response: function (res) {
+                            var config = res.config,
+                                responseBody = res.data,
+                                cache;
+
+                            count--;
+                            // 响应结束，清除相关状态
+                            if (loading && count === 0) {
+                                stopLoading();
                             }
 
-                            return res;
-                        }
-                    },
+                            if (angular.isObject(responseBody) && !responseBody.success) {
 
-                    responseError: function (res) {
-                        $log.error("%s 接口响应失败! 状态：%s 错误信息：%s", res.config.url, res.status, res.statusText);
-                        return $q.reject(res);
+                                // 失败弹出错误提示信息
+                                tipsHandler.error(responseBody.message);
+                                $log.error("%s 接口请求错误：%s", config.url, responseBody.message);
+                                return $q.reject(res);
+
+                            } else {
+
+                                // 自定义配置，用于query请求直接返回data部分
+                                if (config.useDataDirect) {
+                                    res.data = responseBody.data;
+                                }
+
+                                // 自定义配置，若该请求成功后需要重新刷新cache(save,update,delete等操作)，则清空对应cache。angular默认cache为$http
+                                if (config.refreshCache) {
+                                    cache = angular.isObject(config.cache) ? config.cache : $cacheFactory.get("$http");
+                                    cache.removeAll();
+                                }
+
+                                // 关注保存状态则弹出成功提示
+                                if (config.saveStatus) {
+                                    tipsHandler.success(responseBody.message);
+                                }
+
+                                return res;
+                            }
+                        },
+
+                        responseError: function (rejection) {
+                            count--;
+                            // 响应结束，清除相关状态
+                            if (loading && count === 0) {
+                                stopLoading();
+                            }
+                            // 失败弹出错误提示信息
+                            tipsHandler.error("请求错误!");
+                            $log.error("%s 接口响应失败! 状态：%s 错误信息：%s", rejection.config.url, rejection.status, rejection.statusText);
+                            return $q.reject(rejection);
+                        }
                     }
-                }
-
-            }]);
-
+                }]);
         }])
 
-        .run(["$rootScope", "$timeout", "$cacheFactory", function ($rootScope, $timeout, $cacheFactory) {
+        /* 提示信息provider，用于配置错误提示处理器 **/
+        .provider("tipsHandler", function () {
 
-            /** 绑定timeout服务 */
-            _app.timeout = $timeout;
+            var _tipsHandler;
+
+            this.setTipsHandler = function (tipsHandler) {
+
+                _tipsHandler = angular.extend({
+
+                    error  : angular.noop,
+                    warning: angular.noop,
+                    success: angular.noop
+
+                }, tipsHandler);
+            };
+
+            this.error = function (message) {
+                _tipsHandler.error(message);
+            };
+
+            this.warning = function (message) {
+                _tipsHandler.error(message);
+            };
+
+            this.success = function (message) {
+                _tipsHandler.success(message);
+            };
+
+            this.$get = function () {
+                return {
+                    error  : this.error,
+                    warning: this.warning,
+                    success: this.success
+                }
+            };
+
+        })
+
+        .run(["$rootScope", function ($rootScope) {
 
             /** loading状态切换 **/
-            _app.loading = function (flag) {
+            _app.isLoading = function (flag) {
                 $rootScope.loading = flag;
             };
 
-            /** 绑定cache服务 **/
-            _app.cacheFactory = $cacheFactory;
+            _app.isSaving = function (flag) {
+                $rootScope.saving = flag;
+            };
 
         }]);
 
